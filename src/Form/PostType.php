f47ddbf137e84cdd697a77302f3a6031aeb2bef3
Manu Bakala <manu.bakala@gmail.com>
2021-04-21 23:14:27 +0200

    Image uploads works

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,12 +6,20 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (JPG or PNG file)',
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'Supprimer l\'image',
+                'download_uri' => false,
+            ])
             ->add('title')
         ;
     }

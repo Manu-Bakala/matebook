f6fca3a50252f996021e2189e2a9fe2a25a15e5e
Manu Bakala <manu.bakala@gmail.com>
2021-05-02 15:10:32 +0200

    More robust change password form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -12,10 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+/**
+ * @Route("/account")
+ */
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account_show", methods="GET")
+     * @Route("", name="app_account_show", methods="GET")
      */
     public function show(): Response
     {
@@ -25,7 +28,7 @@ class AccountController extends AbstractController
     }
 
     /**
-     * @Route("/account/edit", name="app_account_edit", methods={"GET","POST"})
+     * @Route("/edit", name="app_account_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, EntityManagerInterface $em): Response
     {
@@ -50,13 +53,15 @@ class AccountController extends AbstractController
     }
 
     /**
-     * @Route("/account/change-password", name="app_account_edit_password", methods={"GET","POST"})
+     * @Route("/change-password", name="app_account_edit_password", methods={"GET","POST"})
      */
     public function changePassword(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = $this->getUser(); 
         
-        $form = $this->createForm(ChangePasswordFormType::class);
+        $form = $this->createForm(ChangePasswordFormType::class, null, [
+            'current_password_is_required' => true
+        ]);
 
         $form->handleRequest($request);
 
